a946226af7af8a0c39b1709b40069bf78a71e186
Ahmad MOHADDES <mohaddespour@gmail.com>
2019-02-09 11:18:07 -0500

    Now detects background, capture frames, and display difference

--- motion_detector.py
+++ motion_detector.py
@@ -1,6 +1,6 @@
 import cv2, time
 
-first_frame = None
+background = None
 
 
 video = cv2.VideoCapture(0)
@@ -9,13 +9,17 @@ while True:
     check, frame = video.read()
     
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    gray = cv2.cvtGussianBlur(gray,(21,21),0) 
+    gray = cv2.GaussianBlur(gray,(21,21),0) 
 
-    if first_frame is None
-        first_frame = gray
+    if background is None:
+        background = gray
         continue
+
+    delta_frame = cv2.absdiff(background,gray)
+
     
     cv2.imshow("Captured",gray)
+    cv2.imshow("Difference",delta_frame)
     
     key = cv2.waitKey(1)
     print(gray)
