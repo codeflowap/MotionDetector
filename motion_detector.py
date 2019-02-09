e9b780f928dcbeb9b9c168e1c85a59b1fdeaebcb
Ahmad MOHADDES <mohaddespour@gmail.com>
2019-02-09 16:47:53 -0500

    Added image sample

--- motion_detector.py
+++ motion_detector.py
@@ -2,7 +2,7 @@ import cv2, time, pandas
 from datetime import datetime
 
 first_frame=None
-
+status_list=[None,None]
 times=[]
 df=pandas.DataFrame(columns=["Start","End"])
 
@@ -10,7 +10,7 @@ video=cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
-
+    status=0
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     gray=cv2.GaussianBlur(gray,(21,21),0)
 
@@ -27,9 +27,20 @@ while True:
     for contour in cnts:
         if cv2.contourArea(contour) < 10000:
             continue
+        status=1
 
         (x, y, w, h)=cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 3)
+    status_list.append(status)
+
+    status_list=status_list[-2:]
+
+
+    if status_list[-1]==1 and status_list[-2]==0:
+        times.append(datetime.now())
+    if status_list[-1]==0 and status_list[-2]==1:
+        times.append(datetime.now())
+
 
     cv2.imshow("Gray Frame",gray)
     cv2.imshow("Delta Frame",delta_frame)
@@ -39,7 +50,17 @@ while True:
     key=cv2.waitKey(1)
 
     if key==ord('q'):
+        if status==1:
+            times.append(datetime.now())
         break
+
+print(status_list)
+print(times)
+
+for i in range(0,len(times),2):
+    df=df.append({"Start":times[i],"End":times[i+1]},ignore_index=True)
+
+df.to_csv("Times.csv")
 
 video.release()
 cv2.destroyAllWindows
